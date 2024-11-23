bcd48528d07ba92ec2081c05f2c95fb04a90f4ce
go76yep <louis.skowronek@tum.de>
2024-11-23 14:21:51 +0100

    Added server functionality to post data, such as a dummy-event, to the frontend.

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,17 @@
 #! /usr/bin/python3
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 
 app = Flask(__name__, static_folder='out/_next/static', template_folder='out')
+
+locations = [
+    {
+        'title': 'HackaTUM2024 Demo!',
+        'latitude': 12.3456,
+        'longitude': 34.567
+    }
+]
+
 
 @app.route('/')
 def home():
@@ -15,6 +24,15 @@ def serve_static(path):
 @app.route('/<path:path>')
 def serve_other_static(path):
     return send_from_directory('out', path)
+
+
+@app.route('/api/submit', methods=['POST'])
+def post_data():
+    response = {
+        'locations' : locations
+    }
+
+    return jsonify(response), 200
 
 
 
