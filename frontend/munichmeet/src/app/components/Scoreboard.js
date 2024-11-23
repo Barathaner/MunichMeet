f5f90a6f07c1f351f71f656c1675c81a8bc6edfb
Karl-Augustin Jahnel <freelance@karlaugustinjahnel.de>
2024-11-23 16:58:35 +0100

    added qrcodebutton and scoreboard

---
+++ frontend/munichmeet/src/app/components/Scoreboard.js
@@ -0,0 +1,16 @@
+'use client';
+
+import { useUserPoints } from '../context/context';
+
+export default function Scoreboard() {
+    const { points, name } = useUserPoints();
+
+    return (
+        <div className="absolute top-5 left-3 p-3 bg-gradient-to-r from-yellow-400 to-orange-400 text-black rounded-md shadow-md z-50 font-comic">
+            <h1 className="text-sm font-semibold text-center">MingaCoins</h1>
+            <div className=" flex items-center justify-center">
+                <p className="text-2xl font-bold">{points}</p>
+            </div>
+        </div>
+    );
+}
