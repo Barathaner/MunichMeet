cfb0cbe7d8115872b68c606a6086afd2aa570cab
Karl-Augustin Jahnel <freelance@karlaugustinjahnel.de>
2024-11-23 17:18:50 +0100

    css stuff

--- frontend/munichmeet/src/app/components/Scoreboard.js
+++ frontend/munichmeet/src/app/components/Scoreboard.js
@@ -6,7 +6,7 @@ export default function Scoreboard() {
     const { points, name } = useUserPoints();
 
     return (
-        <div className="absolute bottom-7 left-3 pointer-events-none p-3 bg-gradient-to-r from-yellow-400 to-orange-400 text-black rounded-md shadow-md z-50 font-mono">
+        <div className="absolute bottom-2 left-2 pointer-events-none p-2 bg-gradient-to-r from-yellow-400 to-orange-400 text-black rounded-md shadow-md z-50 font-mono">
             <h1 className="text-sm font-bold text-center">MingaCoins</h1>
             <div className="flex items-center justify-center">
                 <p className="text-2xl font-bold">{points}</p>
--- frontend/munichmeet/src/app/page.js
+++ frontend/munichmeet/src/app/page.js
@@ -104,7 +104,7 @@ export default function Home() {
       {/* QR Code Button */}
       <button
         onClick={() => router.push('/scan')}
-        className="absolute bottom-7 right-4 z-50 w-16 h-16  p-0 bg-yellow-400 hover:opacity-25"
+        className="absolute bottom-2 right-4 z-50 w-16 h-16  p-0 bg-yellow-400 hover:opacity-25"
         style={{ border: 'none', cursor: 'pointer' }}
       >
         <img
