343d8394d9aa3a062de5c46f1031b033506610a9
Karl-Augustin Jahnel <freelance@karlaugustinjahnel.de>
2024-11-23 17:09:56 +0100

    delete fonts

--- frontend/munichmeet/src/app/components/Scoreboard.js
+++ frontend/munichmeet/src/app/components/Scoreboard.js
@@ -6,8 +6,8 @@ export default function Scoreboard() {
     const { points, name } = useUserPoints();
 
     return (
-        <div className="absolute top-5 left-3 p-3 bg-gradient-to-r from-yellow-400 to-orange-400 text-black rounded-md shadow-md z-50 font-comic">
-            <h1 className="text-sm font-semibold text-center">MingaCoins</h1>
+        <div className="absolute top-5 left-3 p-3 bg-gradient-to-r from-yellow-400 to-orange-400 text-black rounded-md shadow-md z-50 font-mono ">
+            <h1 className="text-sm font-bold text-center">MingaCoins</h1>
             <div className=" flex items-center justify-center">
                 <p className="text-2xl font-bold">{points}</p>
             </div>
